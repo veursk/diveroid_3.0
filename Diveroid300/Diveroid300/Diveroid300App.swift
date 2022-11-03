e498f71276d86f632c70f028c3d7e37811f3ddec
DIVEROID-dev <dev@diveroid.com>
2022-11-03 15:26:17 +0900

    시작 전 테스트

---
+++ Diveroid300/Diveroid300/Diveroid300App.swift
@@ -0,0 +1,17 @@
+//
+//  Diveroid300App.swift
+//  Diveroid300
+//
+//  Created by diveroid on 2022/11/03.
+//
+
+import SwiftUI
+
+@main
+struct Diveroid300App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
